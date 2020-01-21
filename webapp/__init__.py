c7f3b23ff6259fd6e5e68f16051b681b72abf359
Puhach Alex <puhach.alex@gmail.com>
2020-01-21 22:47:16 +0200

    Добавили поддрежку БД Flask-SQLAlchemy

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
-
-from webapp.python_org_news import get_python_news
+from webapp.model import db, News
 from webapp.weather import weather_by_city
 
 def create_app():
@@ -9,12 +8,14 @@ def create_app():
 
     app.config.from_pyfile('config.py')
 
+    db.init_app(app)
+
     @app.route("/")
 
     def index():
         title = "Новости Python"
-        weather = weather_by_city("Uzhgorod,Ukraine")
-        news_list = get_python_news()
+        weather = weather_by_city(app.config["WEATHER_DEFAULT_CITY"])
+        news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
 
     return app    
